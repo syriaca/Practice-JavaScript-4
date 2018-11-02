7fd7477df2525b281cec90a49ba48a2c31cc3ecd
Jean-Yves Le Cabellec <jy.lecabellec@gmail.com>
2018-11-02 14:09:36 +0100

    Initial commit
    // 2: Set the color of the <h1> to a different color

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+let heading = document.querySelector('h1');
+
+// 1: Set the text of the <h1> element
+heading.textContent = 'That text have been setted from JS !';
+
+// 2: Set the color of the <h1> to a different color
+heading.style.color = 'rebeccaPurple';
+
+// 3: Set the content of the '.desc' paragraph
+// The content should include at least one HTML tag
+
+
+// 4: Set the class of the <ul> to 'list'
+
+
+// 5: Create a new list item and add it to the <ul>
+
+
+// 6: Change all <input> elements from text fields to checkboxes
+
+
+// 7: Create a <button> element, and set its text to 'Delete'
+// Add the <button> inside the '.extra' <div>
+
+
+// 8: Remove the '.extra' <div> element from the DOM when a user clicks the 'Delete' button
