dcf2e4655aed584d3e1be18d929905536c3ce4b1
Jean-Yves Le Cabellec <jy.lecabellec@gmail.com>
2018-11-02 14:20:36 +0100

    Update js/scripts.js
    // 6: Change all <input> elements from text fields to checkboxes

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ let heading = document.querySelector('h1');
 let description = document.querySelector('.desc');
 let list = document.querySelector('ul');
 let listItem = document.createElement('li');
+let inputCollection = document.querySelectorAll('input');
 
 // 1: Set the text of the <h1> element
 heading.textContent = 'That text have been setted from JS !';
@@ -20,7 +21,7 @@ list.classList.add('list');
 list.append(listItem);
 
 // 6: Change all <input> elements from text fields to checkboxes
-
+inputCollection.forEach(input => input.type = 'checkbox');
 
 // 7: Create a <button> element, and set its text to 'Delete'
 // Add the <button> inside the '.extra' <div>
